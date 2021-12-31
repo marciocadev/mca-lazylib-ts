9d26d47ab0723362e6e2c906e562f2df0ee8ae7d
Marcio Cruz de Almeida <marciocadev@gmail.com>
2021-12-31 13:36:10 -0300

    feat: first commit

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,44 @@
-import { typescript } from "projen";
+import { typescript } from 'projen';
 const project = new typescript.TypeScriptProject({
-  defaultReleaseBranch: "main",
-  name: "mca-lazylib-ts",
+  defaultReleaseBranch: 'main',
+  name: 'mca-lazylib-ts',
   projenrcTs: true,
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  release: true,
+  tsconfig: {
+    compilerOptions: {
+      lib: ['dom', 'es2019'],
+    },
+  },
+
+  deps: ['projen'],
+
+  bundledDeps: [
+    'conventional-changelog-config-spec',
+    'yaml',
+    'fs-extra',
+    'yargs',
+    'case',
+    'glob@^7',
+    'semver',
+    'chalk',
+    '@iarna/toml',
+    'xmlbuilder2',
+    'ini',
+    'shx',
+  ],
+
+  devDeps: [
+    '@types/conventional-changelog-config-spec',
+    '@types/fs-extra@^8',
+    '@types/yargs',
+    '@types/glob',
+    '@types/semver',
+    '@types/ini',
+    'markmac',
+    'all-contributors-cli',
+  ],
 });
+// list all globally installed node.js modules
+// npm ls -g --depth=0
 project.synth();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,91 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
-  }
+// import boxen, { Options } from 'boxen';
+// import chalk from 'chalk';
+import { cdk, FileBase, SourceCode } from 'projen';
+import * as yargs from 'yargs';
+// import { help } from 'yargs';
+// import { hideBin } from 'yargs/helpers';
+
+const project = new cdk.JsiiProject({
+  author: 'Marcio Cruz de Almeida',
+  authorAddress: 'marciocadev@gmail.com',
+  defaultReleaseBranch: 'main',
+  name: 'mca-lazylib',
+  projenrcTs: true,
+  repositoryUrl: 'https://github.com/marciocadev/mca-lazylib.git',
+});
+
+interface EntityType {
+  key: string; type: string;
 }
+
+interface EntityProps {
+  readonly sortKey?: EntityType;
+  readonly fields?: Array<EntityType>;
+}
+
+function createSchema(name: string, partitionKey: EntityType, props?: EntityProps) {
+  const basename = name.toLowerCase();
+  const model = ts(`./${basename}/model.ts`);
+  model.open(`export interface ${name} {`);
+  model.line('/**');
+  model.line(`* **_${partitionKey.key}_** field is the **partition key**`);
+  model.line('*');
+  model.line('* @attribute');
+  model.line('*/');
+  model.line(`readonly ${partitionKey.key}: string; // key`);
+  if (props && props.sortKey) {
+    model.line('/**');
+    model.line(`* **_${props.sortKey.key}_** field is the **sort key**`);
+    model.line('*');
+    model.line('* @attribute');
+    model.line('*/');
+    model.line(`readonly ${props.sortKey.key}: ${props.sortKey.type}; // sort key`);
+  }
+  if (props && props.fields) {
+    for (const field of props.fields) {
+      model.line('/**');
+      model.line('*');
+      model.line('* @attribute');
+      model.line('*/');
+      model.line(`readonly ${field.key}?: ${field.type};`);
+    }
+  };
+  model.close('};');
+}
+
+function ts(path: string) : SourceCode {
+  const src = new SourceCode(project, path);
+  src.line(`// ${FileBase.PROJEN_MARKER}`);
+  return src;
+}
+
+function entity(name: string, partitionKey: EntityType, props?: EntityProps) {
+  // Create Schema
+  createSchema(name, partitionKey, props);
+  // Create Table Construct
+  // createTableConstruct(name, partitionKey, props);
+  // Create client
+  // createClient(name, partitionKey, props);
+}
+
+async function main() {
+  const ya = yargs;
+  ya.recommendCommands();
+  ya.strictCommands();
+  ya.showHelpOnFail(false);
+  ya.wrap(yargs.terminalWidth());
+  ya.option('name', { type: 'string', default: true, desc: 'Name of the model' });
+  ya.completion();
+  ya.help();
+
+  ya.version(false);
+  ya.option('version', { type: 'boolean', description: 'Show version number', global: false });
+
+  const args = await ya.argv;
+  entity(args.name as string, { key: 'username', type: 'string' });
+}
+
+main().catch(e => {
+  console.error(e);
+  process.exit(1);
+});
